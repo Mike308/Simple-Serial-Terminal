64b2cf495888da1f390577aec2283bffdf179525
Majkel44 <m.werner9205@gmail.com>
2017-09-19 18:16:22 +0200

    - added object SerialTerminal - added setting context for qml engine

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "serialterminal.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
+
     QQmlApplicationEngine engine;
+    QQmlContext *context = engine.rootContext();
+    SerialTerminal serialTerminal;
+    context->setContextProperty("serialTerminal",&serialTerminal);
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
 
     return app.exec();
